d856e9194e0c8025f3e16fab1eeaae5a36dd9a41
Aleksander <aleksander@vaehaaland.no>
2025-09-29 15:11:10 +0200

    Configure API project and add EF Core, AutoMapper, and FluentValidation

--- src/api/ProjectHub.Api/Program.cs
+++ src/api/ProjectHub.Api/Program.cs
@@ -1,4 +1,10 @@
 using Serilog;
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using ProjectHub.Infrastructure.Data;
+using ProjectHub.Application.Projects;
+
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,7 +19,16 @@ builder.Host.UseSerilog((context, services, configuration) =>
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddAutoMapper(typeof(ProjectMappingProfile));
+builder.Services
+    .AddFluentValidationAutoValidation()
+    .AddFluentValidationClientsideAdapters();
+builder.Services.AddValidatorsFromAssemblyContaining<CreateProjectDtoValidator>();
+builder.Services.AddValidatorsFromAssemblyContaining<UpdateProjectDtoValidator>();
 builder.Services.AddControllers();
+// TODO: Register AutoMapper, DTO validators, and ProblemDetails config once REST APIs are added.
+builder.Services.AddDbContext<ProjectHubDbContext>(options =>
+  options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
 
 var app = builder.Build();
 
@@ -26,7 +41,9 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
+
+app.UseStatusCodePages();
+// TODO: Plug in exception handling / ProblemDetails middleware before exposing controllers.
 app.MapControllers();
 
 app.Run();
-
